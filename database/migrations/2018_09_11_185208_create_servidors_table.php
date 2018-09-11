f579d626a2864ca72062bb47fba28a67e6fee44f
Antonio Alisson Almeida Queiroz <alisson.almeidaq@gmail.com>
2018-09-11 19:00:48 -0300

    Criando Migretion e Models

--- database/migrations/2018_09_11_185208_create_servidors_table.php
+++ database/migrations/2018_09_11_185208_create_servidors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateServidorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('servidors', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('cpf')->unique();
-            $table->string('password');
-            $table->string('image', 100)->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('servidors');
     }
 }
