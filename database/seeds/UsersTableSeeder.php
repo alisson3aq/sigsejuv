9d538c219b3a2a5f62157f7bfe5628fea06c0d18
Antonio Alisson Almeida Queiroz <alisson.almeidaq@gmail.com>
2018-09-11 19:34:50 -0300

    Criando Usuário Inicial Através de Seeders

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'      => 'ADMIN',
+            'cpf'       => '000.000.000-00',
+            'password'  => bcrypt('123456'),
+        ]);
+     
+    }
+}
